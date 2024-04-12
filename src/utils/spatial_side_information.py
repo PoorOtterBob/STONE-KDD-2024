63be8339f57844146c503a46e7103afbdd6ed52e
Jiaming Ma <131582999+PoorOtterBob@users.noreply.github.com>
2024-04-12 12:26:20 +0800

    Update spatial_side_information.py

--- src/utils/spatial_side_information.py
+++ src/utils/spatial_side_information.py
@@ -10,7 +10,6 @@ from concurrent.futures import ProcessPoolExecutor
 mp.set_start_method('spawn', True)
 
 # set_node_index: set_node_index[0] is observed vertices, set_node_index[-1] is unobserved vertives.
-
 class Spatial_Side_Information():
     def __init__(self, set_node_index, epsilon, c, seed, device):
         super(Spatial_Side_Information, self).__init__()
@@ -34,13 +33,12 @@ class Spatial_Side_Information():
         for i in range(distortion):
             anchor_size = int(math.ceil(n / np.exp2(i + 1)))
             for j in range(sampling_rep_rounds):
-                # anchorset_id[i*sampling_rep_rounds+j].append(self._anchor_node[np.random.choice(n, size=anchor_size, replace=False)])
                 anchorset_id[i*sampling_rep_rounds+j] = np.sort(self._anchor_node[np.random.choice(n, size=anchor_size, replace=False)])
         return anchorset_id, anchorset_num
 
     # node_range  
     # adj: sp.coo_matrix = self._adj
-    # epsilon = self._epsilon 视为字符串相同的忍耐值
+    # epsilon = self._epsilon: Endurance values that are considered to be the same as the string
     def nodes_dist_range(self, adj, node_range):
         dists_dict = defaultdict(dict)
         if False:
@@ -50,12 +48,11 @@ class Spatial_Side_Information():
                     if neighbor not in dists_dict[node]:
                         dists_dict[node][neighbor] = 0
                     diff = (abs(self._adj[node, self._node] - self._adj[neighbor, self._node]) >= self._epsilon)
-                    # diff = ((self._adj[node, self._node] >= self._epsilon) & (self._adj[neighbor, self._node] >= self._epsilon))
                     if isinstance(adj, sp.coo_matrix):
                         dists_dict[node][neighbor] = sp.coo_matrix.sum(diff)
                     elif isinstance(adj, np.ndarray):
                         dists_dict[node][neighbor] = np.sum(diff)
-                    # 这里的邻接矩阵不含self-loop
+                    # no self-loop
                     if self._adj[node, neighbor] > 0:
                         dists_dict[node][neighbor] +=1
         else:
@@ -65,18 +62,16 @@ class Spatial_Side_Information():
                     if neighbor not in dists_dict[node]:
                         dists_dict[node][neighbor] = 0
                     diff = abs(adj[node, self._node] - adj[neighbor, self._node])
-                    # diff = ((self._adj[node, self._node] >= self._epsilon) & (self._adj[neighbor, self._node] >= self._epsilon))
                     if isinstance(adj, sp.coo_matrix):
                         dists_dict[node][neighbor] = sp.coo_matrix.sum(diff)
                     elif isinstance(adj, np.ndarray):
                         dists_dict[node][neighbor] = np.sum(diff)
-                    # 这里的邻接矩阵不含self-loop
+                    # no self-loop
                     if adj[node, neighbor] > 0:
                         dists_dict[node][neighbor] += 1
-            # Wasserstein distance
         return dists_dict
 
-    # 填补字典
+    # Fill in the dictionary
     def merge_dicts(self, dicts):
         result = {}
         for dictionary in dicts:
@@ -102,26 +97,20 @@ class Spatial_Side_Information():
         return dists_dict
 
 
-    # 计算hamming距离dict
+    # Calculate hamming distance dict
     def precompute_dist_data(self, adj):
         dists_array = np.zeros((self._num_nodes, self._num_nodes))
-        # 并行或者不并行
+        # Parallel or not
         dists_dict = self.all_pairs_dist_parallel(adj)
-        # dists_dict = self.nodes_hamming_dist_range(self._node)
         for i, node in enumerate(self._node):
-            #dists_array[i] = list(dists_dict[node].values())
             dists_array[i] = np.array(list(dists_dict[node].values()))
         return dists_array
 
-
-
-    # 得到raw spatial side information
+    # Obtain raw spatial side information
     def get_dist_min(self, adj=None, dist=None):
         anchorset_id, anchorset_num = self.get_random_anchorset()
-        # print(anchorset_id)
         if dist is None:
             dist = self.precompute_dist_data(adj)
-        # dist = self.all_pairs_hamming_dist_parallel()
         dist_min = torch.zeros(self._num_nodes, anchorset_num).to(self._device)
         dist_argmin = torch.zeros(self._num_nodes, anchorset_num).long().to(self._device)
         coefficient = torch.ones(self._num_nodes, anchorset_num).to(self._device)
