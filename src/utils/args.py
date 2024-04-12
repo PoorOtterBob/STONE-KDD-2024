4ebadbae874d0aee8d4379b8fce655668cff333b
Jiaming Ma <131582999+PoorOtterBob@users.noreply.github.com>
2024-04-12 12:16:46 +0800

    Update args.py

--- src/utils/args.py
+++ src/utils/args.py
@@ -18,11 +18,11 @@ def get_public_config():
     parser.add_argument('--output_dim', type=int, default=1)
 
     # For data segment
-    parser.add_argument('--order', type=int, default=0) # 0-位移；1-速递；2-加速度. 考虑总数为1+2*order
+    parser.add_argument('--order', type=int, default=0) # 0-displacement; 1-courier; 2-acceleration and consider the total number 1+2*order (useless)
     parser.add_argument('--new_node_ratio', type=float, default=0.1)
     parser.add_argument('--num_val', type=int, default=1)
-    parser.add_argument('--epsilon', type=float, default=0.01) # 衡量点之间的相似度 for Hamming Metric
-    parser.add_argument('--c', type=int, default=1) # Bourgain定理中Anchorset重复采样次数
+    parser.add_argument('--epsilon', type=float, default=0.01) # Measure similarity between points for Hamming Metric (useless)
+    parser.add_argument('--c', type=int, default=1) # The number of rounds of Anchorset repetitive sampling in Bourgain's theorem
 
 
     # For solving equation
@@ -32,14 +32,14 @@ def get_public_config():
 
     # for traning
     parser.add_argument('--mode', type=str, default='train')
-    parser.add_argument('--test_ratio', type=float, default=0.1) # 0.05, 0.1, 0.15, 0.2
+    parser.add_argument('--test_ratio', type=float, default=0.1) # [0.05, 0.1, 0.15, 0.2]
     parser.add_argument('--ood', type=int, default=1)
     parser.add_argument('--tood', type=int, default=1) # Activated when 
     parser.add_argument('--max_epochs', type=int, default=100)
     parser.add_argument('--patience', type=int, default=10)
-    parser.add_argument('--alpha', type=int, default=0.8)
-    parser.add_argument('--beta', type=int, default=0.1)
-    parser.add_argument('--beta0', type=int, default=2) # L = L_ob + gammaL_un'
+    parser.add_argument('--alpha', type=int, default=0.8) # (useless)
+    parser.add_argument('--beta', type=int, default=0.1) # (useless)
+    parser.add_argument('--beta0', type=int, default=2) # L = L_ob + gammaL_un' (useless)
     return parser
 
 
